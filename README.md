d7bc3ae663f1cde4234704466510cf5186e088e7
Martin Kopecký <kopecky.martin259@gmail.com>
2022-04-15 16:13:56 +0200

    update v 0.0.4

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## Updates
 
+* V 0.0.4
+
+added a main slots and svgs for all platforms. Bootstrap added for responsive styling.
+
 * V 0.0.3
 
 sky bg reworked. Color gradiant changed from (L to R) to (T to B). Moon added to bg.
--- src/layout/main.layout.js
+++ src/layout/main.layout.js
@@ -3,6 +3,9 @@ import anime from 'animejs';
 import Background from "../components/background";
 import Logo from "../components/svg/logo.svg";
 import Parallax from '../middleware/parallax';
+import { Col, Container, Row } from 'reactstrap';
+
+import {Discord, Instagram, Twitter, Twitch, Facebook} from "../assets/@svg/react/solid/";
 
 class Layout extends React.Component {
 
@@ -49,16 +52,82 @@ class Layout extends React.Component {
                                 </center>
                             </div> 
                             <div className='info'>
-                                <center>
-                                <div className='ping'>
-                                    <div className='animation'/>
-                                </div>
-                                </center>
+                                    <Container>
+
+                                    <Row>
+
+                                        <Col lg="6" className='slot-margin'>
+                                            <div className='slot'>
+                                                <div className='title'>
+                                                        Kdo jsem?
+                                                </div>
+                                                <div className='description'>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate, turpis sit amet suscipit ullamcorper, libero leo fermentum erat, interdum viverra nisl dui non nulla. Proin elementum nulla lacus, in suscipit massa fringilla quis. Integer at lorem dictum, luctus nibh quis, pretium tortor. In vestibulum aliquam vestibulum. Nam auctor risus gravida sapien gravida aliquam. Aliquam feugiat eu ligula vitae rhoncus. Praesent in blandit turpis.
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col lg="6" className='slot-margin'>
+                                            <center>
+                                                <img src={process.env.PUBLIC_URL + '/img/logo.jpeg'} /> 
+                                            </center>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col lg="6" className='slot-margin'>
+                                            <Row>
+                                                <Col lg="4" xs="4">
+                                                    <a href="#">
+                                                        <Discord className='svg'/>
+                                                    </a>
+                                                </Col>
+                                                <Col lg="4" xs="4">
+                                                    <a href="https://instagram.com/kopy_tkg" target="_blank">
+                                                        <Instagram className='svg'/>
+                                                    </a>
+                                                </Col>
+                                                <Col lg="4" xs="4">
+                                                    <a href="https://twitter.com/kopy_tkg" target="_blank">
+                                                        <Twitter className='svg'/>
+                                                    </a>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col xs="2" />
+                                                <Col xs="4">
+                                                    <a href="https://www.twitch.tv/kopytkg" target="_blank">
+                                                        <Twitch className='svg'/>
+                                                    </a>
+                                                </Col>
+                                                <Col xs="4">
+                                                    <a href='https://www.facebook.com/Kopyy' target="_blank">
+                                                        <Facebook className='svg' />
+                                                    </a>
+                                                </Col>
+                                                <Col xs="2" />
+                                            </Row>
+                                        </Col>
+                                        <Col lg="6" className='slot-margin'>
+                                            <div className='slot'>
+                                                <div className='title'>
+                                                    Kde mě najdeš?
+                                                </div>
+                                                <div className='description'>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate, turpis sit amet suscipit ullamcorper, libero leo fermentum erat, interdum viverra nisl dui non nulla. Proin elementum nulla lacus, in suscipit massa fringilla quis. Integer at lorem dictum, luctus nibh quis, pretium tortor. In vestibulum aliquam vestibulum. Nam auctor risus gravida sapien gravida aliquam. Aliquam feugiat eu ligula vitae rhoncus. Praesent in blandit turpis.
+                                                </div>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    </Container>
                             </div>
                         </div>
+                        <siv className="splitter">
+                            <svg viewBox="0 0 1440 320">
+                                <path  fill-opacity="1" d="M0,160L48,160C96,160,192,160,288,170.7C384,181,480,203,576,229.3C672,256,768,288,864,266.7C960,245,1056,171,1152,117.3C1248,64,1344,32,1392,16L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+                            </svg>
+                        </siv>
                     </div>
                 </div>
-
+                <div className='day'></div>
                 <Background />
             </>
 
