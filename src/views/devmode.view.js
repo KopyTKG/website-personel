045c3b072999279d3419d78193550f4a2194af2f
Martin Kopecký <kopecky.martin259@gmail.com>
2022-07-14 13:44:47 +0200

    dev merger

--- src/views/devmode.view.js
+++ src/views/devmode.view.js
@@ -6,12 +6,14 @@ const DevView = () => {
         {
             id: 0,
             title: "CS: Source",
-            command: "-novid -tickrate 128 -w 1920 -h 1080 +fps_max 0"
+            command: "-novid -tickrate 128 -w 1920 -h 1080 +fps_max 0",
+            config: "https://raw.githubusercontent.com/KopyTKG/CONFIG-STORE/main/css.cfg"
         },
         {
             id: 1,
             title: "CS:GO",
-            command: "-nojoy -fullscreen -novid -tickrate 128 -w 1920 -h 1080 +fps_max 0"
+            command: "-nojoy -fullscreen -novid -tickrate 128 -w 1920 -h 1080 +fps_max 0",
+            config: "https://raw.githubusercontent.com/KopyTKG/CONFIG-STORE/main/csgo.cfg"
         }
     ]);
 
@@ -27,7 +29,10 @@ const DevView = () => {
                 return(<>
                     <div className='code' key={row.id + key+ Date.now()}>
                         <h2>{row.title}</h2>
-                        <code onClick={() => handleCopy(row.command)} children={row.command}/>
+                        <code onClick={() => handleCopy(row.command)} children={row.command}/> 
+                        <br/>
+                        <br/>
+                        <a className='file' href={row.config} download target="_blank">DOWNLOAD CONFIG</a>
                     </div>
                 </>);
             })
