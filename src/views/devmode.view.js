d76ebfe5346bcd683d7836dda40eced520dc6007
Martin "Kopy" Kopecký <47574331+KopyTKG@users.noreply.github.com>
2022-11-04 18:01:17 +0100

    Development (#16)
    * devmode rework
    
    * UI rework
    
    Co-authored-by: Martin Kopecký <kopecky.martin259@gmail.com>

--- src/views/devmode.view.js
+++ src/views/devmode.view.js
@@ -24,6 +24,7 @@ const DevView = () => {
             command: "0;s;1;P;c;8;u;FF4D00FF;o;1;d;1;b;1;z;5;f;0;s;0;0t;3;0l;3;0v;5;0o;0;0a;1;0m;1;0s;0.1;1b;0;S;b;1;c;8;t;FF4D00FF;s;2;o;0.5",
         }
     ]);
+    
 
     const handleCopy = (copy) => {
         navigator.clipboard.writeText(copy);
