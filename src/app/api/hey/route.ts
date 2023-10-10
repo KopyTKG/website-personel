012afd3b3743323bb84b5cfcec30a4ff5eb874c8
Martin Kopecký <kopecky@thekrew.app>
2023-10-10 15:56:06 +0200

    CRUD api added

--- src/app/api/hey/route.ts
+++ src/app/api/hey/route.ts
@@ -2,37 +2,42 @@ import prisma from '../../../../prisma/client'
 import JWT from '@/modules/controllers/jwt.controller'
 import * as jose from 'jose'
 
+
+async function GetAuth(header: any) {
+    const auth = String(header.get('authorization')?.split(" ")[1])
+    try {
+        const jwt = new JWT()
+        const secret = await jwt.getPublic()
+        const { payload } = await jose.jwtVerify(auth, secret, {
+            issuer: 'urn:thekrew:issuer',
+            audience: 'urn:thekrew:audience',
+        })
+
+        return true 
+    } catch (e) {
+        return false
+    }
+}
+
+
 export async function POST(
     req: Request,
   ){
     try{
         const headers = req.headers
-        const token = String(headers.get('authorization')?.split(" ")[1])
+        const auth = await GetAuth(headers);    
         const settings = await req.json();
-
-        // JWT stuff
-        const jwt = new JWT();
-        const secret = await jwt.getPublic();
-
-        try {
-            
-            const { payload, protectedHeader } = await jose.jwtVerify(token , secret, {
-                issuer: 'urn:thekrew:issuer',
-                audience: 'urn:thekrew:audience',
-            })
-            
-            if(payload) {
-                await prisma.heyCount.create({
-                    data: {
-                        count: Number(settings.data),
-                        
-                    }
-                });
-                return Response.json("Done")                
-            }
-        } catch(e) {
-            return Response.json(JSON.stringify('Invalid Token'))
+        if(!auth) {
+            return Response.json('Invalid Token')
+        } else {
+            await prisma.heyCount.create({
+                data: {
+                    count: Number(settings.data),
+                }
+            });
+            return Response.json("Done")                
         }
+        
     } catch (e) {
         return Response.json('Internal Server Error')
     }
@@ -43,35 +48,74 @@ export async function GET(
 ){
     try{
         const headers = req.headers
-        const token = String(headers.get('authorization')?.split(" ")[1])
-    
-        // JWT stuff
-        const jwt = new JWT();
-        const secret = await jwt.getPublic();
-
-        try {
-            const { payload, protectedHeader } = await jose.jwtVerify(token , secret, {
-                issuer: 'urn:thekrew:issuer',
-                audience: 'urn:thekrew:audience',
-            })
-            if(payload) {
-                const data = await prisma.heyCount.findFirst(
-                    {
-                        orderBy: {
-                            createdAt: 'desc'
-                        }
-                    }
-                )
-                if (data == null) {
-                    return Response.json(0)
-                } 
-                return Response.json(data)                
-            }
-        } catch (e) {
+        const auth = await GetAuth(headers);
+        if(!auth) {
             return Response.json('Invalid Token')
+        } else {
+            const data = await prisma.heyCount.findFirst(
+                {
+                    orderBy: {
+                        createdAt: 'desc'
+                    }
+                }
+            )
+            if (data == null) {
+                return Response.json(0)
+            } 
+            return Response.json(data)                
         }
+        
     } catch (e) {
         return Response.json('Internal Server Error')
     }
-    return Response.json('Hey')
+}
+
+export async function PUT(
+    req: Request,
+    ){
+      try{
+          const headers = req.headers
+          const settings = await req.json();
+          const auth = await GetAuth(headers);    
+          if(!auth) {
+              return Response.json('Invalid Token')
+          } else {
+              await prisma.heyCount.update({
+                where: {
+                    id: settings.id,
+                },
+                data: {
+                      count: Number(settings.data),
+                      
+                  }
+              });
+              return Response.json(`Row ${settings.id} was updated`)                
+          }
+          
+      } catch (e) {
+          return Response.json('Internal Server Error')
+      }   
+}
+
+export async function DELETE(
+    req: Request
+){
+    try {
+        const headers = req.headers
+        const settings = await req.json();
+        const auth = await GetAuth(headers);    
+        if(!auth) {
+            return Response.json('Invalid Token')
+        } else {
+            await prisma.heyCount.delete({
+              where: {
+                  id: settings.id,
+              },
+            });
+            return Response.json(`Row ${settings.id} was deleted`)                
+        }
+        
+    } catch (e) {
+        return Response.json('Internal Server Error')
+    }
 }
