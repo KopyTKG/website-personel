ce8cdd05d428625b22a3ecfb3a6f172380e12284
Martin Kopecký <kopecky@thekrew.app>
2024-03-26 16:59:10 +0100

    prettier fixed and adding last copy buttons

--- src/components/sections/gui/chapter6.tsx
+++ src/components/sections/gui/chapter6.tsx
@@ -367,7 +367,60 @@ export async function POST(req: Request) {
       Next we will get instance of our <code>RedisLib</code> and check if we have <code>id</code> in
       cache. Based on that we will return the url or throw 404 error code
      </H>
-     <Code>
+     <Code text={`import RedisLib from '@/lib/redis'
+
+function getRandomNumber(): number {
+    const min = 1;
+    const max = 1099511627775
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export async function GET(req: Request){
+    try {
+        const path = new URL(req.url)
+        const id = path.searchParams.get('id')
+        if (!id) throw new Error('No id provided')
+
+        const redis = RedisLib.getInstance().getClient();
+        
+        let url = await redis.get(id);
+
+        if (id) {
+            return new Response(JSON.stringify({url: url}), { status: 200 })
+        } else {
+            return new Response(JSON.stringify({message: 'No url found'}), { status: 404 })
+        }
+
+    } catch (error) {
+        return new Response('Error', { status: 500 })
+    }
+}
+
+
+export async function POST(req: Request) {
+    try {
+        const data = await req.json()
+        const { url } = data
+        
+        const redis = RedisLib.getInstance().getClient();
+
+        let id = await redis.get(url);
+
+        if (id) {
+            return new Response(JSON.stringify({id: id}), { status: 200 })
+        } else {
+   
+            let id = getRandomNumber().toString(16)
+            
+            await redis.set(id, url, 'EX', 60 * 60 * 4)
+            await redis.set(url, id, 'EX', 60 * 60 * 4)
+            
+            return new Response(JSON.stringify({id: id}), { status: 200 })
+        }
+    } catch (error) {
+        return new Response('Error', { status: 500 })
+    }
+}`}>
       <SyntaxHighlighter language="typescript" style={irBlack}>
        {`import RedisLib from '@/lib/redis'
 
@@ -431,7 +484,7 @@ export async function POST(req: Request) {
       But because we use dynamic route that calls the api. We can run into caching proble. To solve
       that we will turn caching off with this constant.
      </H>
-     <Code>
+     <Code text={`export const dynamic = 'force-dynamic'`}>
       <SyntaxHighlighter language="typescript" style={irBlack}>
        {`export const dynamic = 'force-dynamic'`}
       </SyntaxHighlighter>
@@ -439,7 +492,62 @@ export async function POST(req: Request) {
     </div>
     <div>
      <H>We need to set it at the begining of our code just like this</H>
-     <Code>
+     <Code
+      text={`import RedisLib from '@/lib/redis'
+export const dynamic = 'force-dynamic'
+
+function getRandomNumber(): number {
+    const min = 1;
+    const max = 1099511627775
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export async function GET(req: Request){
+    try {
+        const path = new URL(req.url)
+        const id = path.searchParams.get('id')
+        if (!id) throw new Error('No id provided')
+
+        const redis = RedisLib.getInstance().getClient();
+        
+        let url = await redis.get(id);
+
+        if (id) {
+            return new Response(JSON.stringify({url: url}), { status: 200 })
+        } else {
+            return new Response(JSON.stringify({message: 'No url found'}), { status: 404 })
+        }
+
+    } catch (error) {
+        return new Response('Error', { status: 500 })
+    }
+}
+
+
+export async function POST(req: Request) {
+    try {
+        const data = await req.json()
+        const { url } = data
+        
+        const redis = RedisLib.getInstance().getClient();
+
+        let id = await redis.get(url);
+
+        if (id) {
+            return new Response(JSON.stringify({id: id}), { status: 200 })
+        } else {
+   
+            let id = getRandomNumber().toString(16)
+            
+            await redis.set(id, url, 'EX', 60 * 60 * 4)
+            await redis.set(url, id, 'EX', 60 * 60 * 4)
+            
+            return new Response(JSON.stringify({id: id}), { status: 200 })
+        }
+    } catch (error) {
+        return new Response('Error', { status: 500 })
+    }
+}`}>
       <SyntaxHighlighter language="typescript" style={irBlack}>
        {`import RedisLib from '@/lib/redis'
 export const dynamic = 'force-dynamic'
