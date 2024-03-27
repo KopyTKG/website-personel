31a4275c03833bfc85b178615ea1d3e86a85541f
Martin Kopecký <kopecky@thekrew.app>
2024-03-27 12:10:10 +0100

    nav moved to fixed

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster'
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
  return (
   <div className="min-h-screen">
-   <nav className="sticky top-0 h-14 bg-zinc-900/95 min-w-full z-50">
+   <nav className="fixed top-0 h-14 bg-zinc-900/95 min-w-full z-50">
     <div className="flex justify-between h-full text-white max-w-6xl mx-auto items-center px-10 xl:px-0 ">
      <div className="flex gap-5">
       <a
