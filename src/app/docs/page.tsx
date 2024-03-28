c8b8b26df6d7823ba6518364409c9448d5b990cd
Martin Kopecký <kopecky@thekrew.app>
2024-03-28 18:34:42 +0100

    fixing layout

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,6 +1,6 @@
 export default function Docs() {
  return (
-  <div className="flex justify-center min-w-full h-11 md:grid md:grid-cols-2 xl:grid-cols-3 pt-5">
+  <div className="flex justify-center min-w-full h-11 md:grid md:grid-cols-2 xl:grid-cols-3 pt-14">
    <div className="text-justify flex flex-col gap-3 bg-black/65 px-3 py-4 max-w-md h-max border rounded-xl">
     <div className="flex justify-between items-center">
      <h1 className="text-3xl font-bold">Next.JS</h1>
