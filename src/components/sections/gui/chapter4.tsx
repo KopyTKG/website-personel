89ebd3a98527eda7684acad751ff6f738a5f6406
Martin Kopecký <kopecky@thekrew.app>
2024-04-03 17:22:44 +0200

    text reform

--- src/components/sections/gui/chapter4.tsx
+++ src/components/sections/gui/chapter4.tsx
@@ -61,8 +61,7 @@ return(<h1>Hello World</h1>)
     </div>
     <div>
      <H>
-      Styling is done, now we need logic to make it do things. Let&apos;s code that. Add async
-      function named{' '}
+      Styling is done, now we need to add some logic to make it do things. Add async function named{' '}
       <code>
        <HL>Submit</HL>
       </code>{' '}
@@ -104,40 +103,40 @@ return (
     <div>
      <H>
       And we need to link it to buttons <code>onClick</code> event, but by doing so we need to
-      switch into client side rendering by adding <code>{`'use client'`}</code> flag and the start
+      switch into client side rendering by adding <code>{`'use client'`}</code> flag at the start
       and now we can link the <code>onClick</code>
      </H>
      <Code
       text={`'use client'
 export default function Home() {
-async function Submit() {}
-return (
-  <main className="w-full h-[95vh] flex justify-center items-center">
-    <div className=" flex flex-col items-center w-1/4 h-fit outline-1 outline outline-gray-500 bg-gray-900 rounded-lg gap-10 py-5 px-3">
-      <h1 className="text-3xl text-white font-bold" > ShortURL </h1>
-      <input placeholder="Enter URL to shorten" className="w-full h-10 px-3 rounded-lg bg-slate-600 text-white" />
-      <button className="text-xl bg-blue-400 rounded-xl px-4 py-2 text-black hover:bg-blue-700 hover:text-white ease-in-out duration-300" onClick={() => Submit()}>
-        Create URL
-      </button>
-    </div>
-  </main>
-);
+  async function Submit() {}
+  return (
+    <main className="w-full h-[95vh] flex justify-center items-center">
+      <div className=" flex flex-col items-center w-1/4 h-fit outline-1 outline outline-gray-500 bg-gray-900 rounded-lg gap-10 py-5 px-3">
+        <h1 className="text-3xl text-white font-bold" > ShortURL </h1>
+        <input placeholder="Enter URL to shorten" className="w-full h-10 px-3 rounded-lg bg-slate-600 text-white" />
+        <button className="text-xl bg-blue-400 rounded-xl px-4 py-2 text-black hover:bg-blue-700 hover:text-white ease-in-out duration-300" onClick={() => Submit()}>
+          Create URL
+        </button>
+      </div>
+    </main>
+  );
 }`}>
       <SyntaxHighlighter language="typescript" style={irBlack}>
        {`'use client'
 export default function Home() {
-async function Submit() {}
-return (
-  <main className="w-full h-[95vh] flex justify-center items-center">
-    <div className=" flex flex-col items-center w-1/4 h-fit outline-1 outline outline-gray-500 bg-gray-900 rounded-lg gap-10 py-5 px-3">
-      <h1 className="text-3xl text-white font-bold" > ShortURL </h1>
-      <input placeholder="Enter URL to shorten" className="w-full h-10 px-3 rounded-lg bg-slate-600 text-white" />
-      <button className="text-xl bg-blue-400 rounded-xl px-4 py-2 text-black hover:bg-blue-700 hover:text-white ease-in-out duration-300" onClick={() => Submit()}>
-        Create URL
-      </button>
-    </div>
-  </main>
-);
+  async function Submit() {}
+  return (
+    <main className="w-full h-[95vh] flex justify-center items-center">
+      <div className=" flex flex-col items-center w-1/4 h-fit outline-1 outline outline-gray-500 bg-gray-900 rounded-lg gap-10 py-5 px-3">
+        <h1 className="text-3xl text-white font-bold" > ShortURL </h1>
+        <input placeholder="Enter URL to shorten" className="w-full h-10 px-3 rounded-lg bg-slate-600 text-white" />
+        <button className="text-xl bg-blue-400 rounded-xl px-4 py-2 text-black hover:bg-blue-700 hover:text-white ease-in-out duration-300" onClick={() => Submit()}>
+          Create URL
+        </button>
+      </div>
+    </main>
+  );
 }`}
       </SyntaxHighlighter>
      </Code>
@@ -146,7 +145,7 @@ return (
      <H>
       Accessing data of input field can be tricky. To make it easier for us we&apos;re gonna use{' '}
       <code>useRef</code> hook, but first we need to import it from <code>react</code>. Then create
-      instance and link it to the input field. Code should be like this
+      instance and link it to the input field. Code should look like this
      </H>
      <Code
       text={`'use client'
