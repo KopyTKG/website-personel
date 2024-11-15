12c6687869a765a20dde2eaf0d2d06610669c35f
Martin Kopecký <kopecky@thekrew.app>
2024-11-15 19:55:12 +0100

    roll back

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  transpilePackages: ['lucide-react'], // add this
@@ -17,6 +30,10 @@ const nextConfig = {
      {
       key: 'Access-Control-Allow-Headers',
       value: 'Content-Type, Authorization',
+     },
+     {
+      key: 'Content-Security-Policy',
+      value: cspHeader.replace(/\n/g, ''),
      },
      {
       key: 'X-Frame-Options',
