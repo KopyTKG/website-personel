c275cc73bd36b7c12473cca1e9947f3def34e564
Martin Kopecký <kopecky@thekrew.app>
2024-11-15 19:25:01 +0100

    testing CSP headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  transpilePackages: ['lucide-react'], // add this
+ headers: [
+  {
+   key: 'Content-Security-Policy',
+   value: cspHeader.replace(/\n/g, ''),
+  },
+ ],
 }
 
 module.exports = nextConfig
