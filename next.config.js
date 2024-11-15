b1fa8d4044466e0a6b405fc0f7d3088969ef556b
Martin Kopecký <kopecky@thekrew.app>
2024-11-15 19:32:11 +0100

    my bad with config

--- next.config.js
+++ next.config.js
@@ -14,28 +14,35 @@ const cspHeader = `
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  transpilePackages: ['lucide-react'], // add this
- headers: [
-  {
-   key: 'Content-Security-Policy',
-   value: cspHeader.replace(/\n/g, ''),
-  },
-  {
-   key: 'X-Frame-Options',
-   value: 'SAMEORIGIN',
-  },
-  {
-   key: 'X-Content-Type-Options',
-   value: 'nosniff',
-  },
-  {
-   key: 'Referrer-Policy',
-   value: 'origin-when-cross-origin',
-  },
-  {
-   key: 'Permissions-Policy',
-   value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
-  },
- ],
+ async headers() {
+  return [
+   {
+    source: '/',
+    headers: [
+     {
+      key: 'Content-Security-Policy',
+      value: cspHeader.replace(/\n/g, ''),
+     },
+     {
+      key: 'X-Frame-Options',
+      value: 'SAMEORIGIN',
+     },
+     {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff',
+     },
+     {
+      key: 'Referrer-Policy',
+      value: 'origin-when-cross-origin',
+     },
+     {
+      key: 'Permissions-Policy',
+      value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+     },
+    ],
+   },
+  ]
+ },
 }
 
 module.exports = nextConfig
